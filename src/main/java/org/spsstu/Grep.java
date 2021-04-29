71f1c123639ce6264e0a25070034c608ba844da0
gh1352900923 <bob@example.com>
2021-04-29 17:17:03 +0300

    123

--- src/main/java/org/spsstu/Grep.java
+++ src/main/java/org/spsstu/Grep.java
@@ -1,45 +1,48 @@
+package org.spsstu;
+
 import java.io.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Grep {
+class Grep {
     private Boolean invert = false;
     private Boolean ignore = false;
     private Boolean regex = false;
     private String word;
-    private final String fileName;
-    private final StringBuilder result = new StringBuilder();
+    private final File file;
 
-    public Grep(Boolean inv, Boolean ign, Boolean reg, String theWord, String file) throws IOException {
+    Grep(Boolean inv, Boolean ign, Boolean reg, String theWord, File file) {
         if (inv != null) this.invert = inv;
         if (ign != null) this.ignore = ign;
         if (reg != null) this.regex = reg;
         word = theWord;
-        fileName = file;
+        this.file = file;
     }
-    public String grep() throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(fileName));
+    String grep() throws IOException {
+        StringBuilder result = new StringBuilder();
+        BufferedReader reader = new BufferedReader(new FileReader(file));
         String line = reader.readLine();
-        boolean flag = false;
+        Pattern pattern = Pattern.compile(word);
         while (line != null) {
+            boolean flag = false;
             if (ignore) {
                 line = line.toLowerCase();
-                if (!regex) word = word.toLowerCase();
+                word = word.toLowerCase();
             }
             if (regex) {
-                Pattern pattern = Pattern.compile(word);
                 Matcher matcher = pattern.matcher(line);
                 flag = matcher.find();
             }
             else if (line.contains(word)) {
                 flag = true;
             }
-            if (ignore) {
+            if (invert) {
                 flag = !flag;
             }
             if (flag) result.append(line).append("\n");
             line = reader.readLine();
         }
+        reader.close();
         return result.toString();
     }
 }
--- src/main/java/org/spsstu/GrepLauncher.java
+++ src/main/java/org/spsstu/GrepLauncher.java
@@ -1,6 +1,8 @@
+package org.spsstu;
+
 import org.kohsuke.args4j.*;
 
-import java.io.FileNotFoundException;
+import java.io.File;
 import java.io.IOException;
 
 /* Вывод на консоль указанного (в аргументе командной строки) текстового файла с фильтрацией
@@ -19,21 +21,18 @@ public class GrepLauncher {
     private Boolean invertConditions;
     @Option(name = "-i", metaVar = "IgnoreRegister")
     private Boolean ignoreRegister;
-
     @Option(name = "-r", metaVar = "Regex")
     private Boolean regexInsteadWord;
-
     @Argument(required = true, usage = "Word to search")
     private String theSearchedWord;
+    @Argument(required = true,usage = "Input file name", index = 1)
+    private File inputFileName;
 
-    @Argument(usage = "Input file name", index = 1)
-    private String inputFileName;
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         new GrepLauncher().launch(args);
     }
 
-    private void launch(String[] args) {
+    private void launch(String[] args) throws  IOException {
         CmdLineParser parser = new CmdLineParser(this);
         try {
             parser.parseArgument(args);
@@ -41,13 +40,12 @@ public class GrepLauncher {
             System.err.println(e.getMessage());
             System.err.println("java -jar grep [-v] [-i] [-r] word inputname.txt");
             parser.printUsage(System.err);
+            return;
         }
-        try {
-            Grep grep = new Grep(invertConditions, ignoreRegister, regexInsteadWord, theSearchedWord, inputFileName);
-            System.out.println(grep.grep());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        Grep grep = new Grep(invertConditions, ignoreRegister, regexInsteadWord, theSearchedWord, inputFileName);
+        System.out.println(grep.grep());
+
+
     }
 
 
